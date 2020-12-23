084674b161d51c4b502fe6370d422f22ad51e093
Yangnom <ynom@pm.me>
2020-12-22 17:52:05 -0800

    Define custom actions and print to test.

--- notifications example/ContentView.swift
+++ notifications example/ContentView.swift
@@ -79,6 +79,7 @@ struct ContentView: View {
                     print(error.localizedDescription)
                 }
             }
+            defineCustomActions()
         })
         
     }
--- notifications example/TestingView.swift
+++ notifications example/TestingView.swift
@@ -13,22 +13,48 @@ struct TestingView: View {
     @State var anArray: [String] = ["First", "Second"]
     
     var body: some View {
-        VStack{
-            ForEach(anArray, id: \.self) { element in
-                Text("Here is a strig: \(element)")
-            }
-            Button(
-                action: {
-//                    self.anArray = ["This", "is", "just", "a", "test"]
-//                    self.anArray.append(contentsOf: anArrayMadeByAFunction())
-                    self.anArray += anArrayMadeByAFunction()
-                },
-                label: { Text("Change it!") }
-            )
+        Button("Print Categories") {
+            printCategories()
+        }
+    }
+}
+    
+
+func defineCustomActions() {
+    // Define the custom actions.
+    let acceptAction = UNNotificationAction(identifier: "ACCEPT_ACTION",
+                                            title: "Accept",
+                                            options: UNNotificationActionOptions(rawValue: 0))
+    let declineAction = UNNotificationAction(identifier: "DECLINE_ACTION",
+                                             title: "Decline",
+                                             options: UNNotificationActionOptions(rawValue: 0))
+    // Define the notification type
+    let meetingInviteCategory =
+        UNNotificationCategory(identifier: "MEETING_INVITATION",
+                               actions: [acceptAction, declineAction],
+                               intentIdentifiers: [],
+                               hiddenPreviewsBodyPlaceholder: "",
+                               options: .customDismissAction)
+    // Register the notification type.
+    let notificationCenter = UNUserNotificationCenter.current()
+    notificationCenter.setNotificationCategories([meetingInviteCategory])
+    notificationCenter.getNotificationCategories{ setOfCategories in
+        for category in setOfCategories {
+            print(category)
         }
     }
 }
 
+    func printCategories() {
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.getNotificationCategories{ setOfCategories in
+            for category in setOfCategories {
+                print(category)
+            }
+            print("There are \(setOfCategories.count) categories")
+        }
+    }
+    
 func anArrayMadeByAFunction() -> [String] {
     return ["This", "was", "made", "from", "a", "function"]
 }
